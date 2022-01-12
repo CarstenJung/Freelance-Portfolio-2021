da87be024e106a73ab2431662a609cc67e26ec9d
Carsten Jung <39696700+CarstenJung@users.noreply.github.com>
2022-01-12 23:19:26 +0100

    Menu Animation finished (for now)

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,7 +1,84 @@
+import gsap from "gsap"
+/* PORTFOLIO SLIDER ANIMATION MENU */
+let menuProjectItems = gsap.timeline();
+
+menuProjectItems
+  .to("#menuProjectsItem1", {
+    opacity: 1,
+    display: "block",
+    duration: 3
+  })
+  .to("#menuProjectsItem1", {
+    opacity: 0,
+    display: "none",
+    duration: 3
+  })
+  .to("#menuProjectsItem2", {
+    opacity: 1,
+    display: "block",
+    duration: 3
+  })
+  .to("#menuProjectsItem2", {
+    opacity: 0,
+    display: "none",
+    duration: 3
+  })
+  .to("#menuProjectsItem3", {
+    opacity: 1,
+    display: "block",
+    duration: 3
+  })
+  .to("#menuProjectsItem3", {
+    opacity: 0,
+    display: "none",
+    duration: 3
+  });
+menuProjectItems.repeat(-1);
+
 (function () {
   document.querySelectorAll('.burgerMenu').forEach(btn => {
     btn.addEventListener('click', e => {
       btn.classList.toggle('active');
+
+      /* PORTFOLIO SLIDER ANIMATION MENU */
+      const menuOverlay = document.querySelector(".burgerMenu");
+
+      if (menuOverlay.classList.contains("active")) {
+        gsap.to(".menuOverlay", {
+          opacity: 1,
+          display: "flex",
+          duration: 1,
+          zIndex: 1
+        });
+        gsap.to(".menuFlexContainer ul li", {
+          y: 0,
+          stagger: 0.1,
+          duration: 2
+        })
+        gsap.to(".menuFlexContainer h1, .officeFlexContainer h1", {
+          opacity: .3,
+          x: 0,
+          duration: 3
+        })
+        menuProjectItems.play()
+      } else {
+        gsap.to(".menuOverlay", {
+          opacity: 0,
+          display: "none",
+          duration: 1
+        });
+        gsap.to(".menuFlexContainer ul li", {
+          y: -100,
+          stagger: 0.1,
+          duration: 2
+        })
+        gsap.to(".menuFlexContainer h1, .officeFlexContainer h1", {
+          opacity: 0,
+          x: -100,
+          duration: 3
+        })
+        menuProjectItems.pause()
+      }
 
       setTimeout(() => {
         const pathId = document.querySelector('.pathSVG')
