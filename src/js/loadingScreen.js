3cee85bd640937620db7ad527998f54a368ac0dd
Carsten Jung <39696700+CarstenJung@users.noreply.github.com>
2022-03-16 11:12:49 +0100

    final Laptop responsive check

--- src/js/loadingScreen.js
+++ src/js/loadingScreen.js
@@ -1,12 +1,20 @@
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+
+gsap.registerPlugin(DrawSVGPlugin);
+
+
 var startAnimation = gsap.timeline({
   delay: .5
 });
+
+/* gsap.set('.staggerItem', {perspective: '1200px'}) */
 
 startAnimation.fromTo('#CTop', {drawSVG: '0%'}, {drawSVG: '100%', duration: 1})
 .fromTo('#CBottom', {drawSVG: '0%'}, {drawSVG: '100%', duration: 1})
 .fromTo('#JTop', {drawSVG: '0%'}, {drawSVG: '100%', duration: 1})
 .to('#CTop, #CBottom', {fill: '#fff', duration: 2})
-.to('#JTop', {fill: '#ffcd48', duration: 2}, '-=2')
-//.to('.circle', {scale: 1, duration: 1.3})
-.to('.startAnimationWrapper', {opacity: 0, duration: 1})
-.to('.animationBcg', {opacity: 0, duration: 1}, '-=1')
+.to('#JTop', {fill: '#ffcd48', duration: 2.3}, '-=2')
+.to('.startAnimationWrapper', {autoAlpha: 0, duration: 2})
+.to('.staggerItem', {rotationY: -90, stagger: .1, ease: "power1.out", transformOrigin: "center center"}, '-=0.6')
+.to('.staggerAnimation, .animationBcg', { display: 'none' })
